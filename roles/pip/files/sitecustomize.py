ae98a2ed7d92e0eb2682ec13cc83ca46893d3329
qinwenchao <qinwenchao@chunyu.me>
2017-12-22 17:21:08 +0800

    update

---
+++ roles/pip/files/sitecustomize.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# Do NOT modify this file by hand!
+# Generate by ansible
+# 设置系统的默认编码, 这样utf-8和unicode之间就可以自由转换了；否则系统默认的编码为ascii
+#
+import sys
+sys.setdefaultencoding('utf-8')
